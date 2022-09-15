178a248b2b28302defce77ae37b6674326b6530b
AnggaPutraa <yananggaa@gmail.com>
2022-09-15 12:56:09 +0700

    lab 2 setup

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,9 +1,14 @@
 
 from django.urls import path
-from wishlist.views import show_wishlist
+from wishlist.views import show_wishlist, show_wishlist_xml, show_wishlist_json, show_json_by_id, show_xml_by_id
 
 app_name = 'wish_list'
 
 urlpatterns = [
     path('', show_wishlist, name= 'show_wishlist'),
+    path('xml/', show_wishlist_xml, name='show_wishlist_xml'),
+    path('json/', show_wishlist_json, name='show_wishlist_json'),
+    path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
+    path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
+    path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,13 +1,49 @@
-from multiprocessing import context
 from django.shortcuts import render
 from wishlist.models import BarangWishList
+from django.http import HttpResponse
+from django.core import serializers
 
-wishlist_data_barang = BarangWishList.objects.all()
-context = {
-    'list_barang' : wishlist_data_barang,
-    'nama' : 'Kak Cinoy'
-}
-
-# Create your views here.
 def show_wishlist(request):
-    return render(request, "wishlist.html", context)
+    wishlist_data_barang = BarangWishList.objects.all()
+    context = {
+        'list_barang' : wishlist_data_barang,
+        'nama' : 'Kak Cinoy'
+    }
+
+    return render(
+        request, 
+        "wishlist.html", 
+        context
+    )
+
+def show_wishlist_xml(request):
+    data = BarangWishList.objects.all()
+
+    return HttpResponse(
+        serializers.serialize("xml", data), 
+        content_type="application/xml"
+    )
+
+def show_wishlist_json(request):
+    data = BarangWishList.objects.all()
+
+    return HttpResponse(
+        serializers.serialize("json", data), 
+        content_type="application/json"
+    )
+
+def show_json_by_id(request, id):
+    data = BarangWishList.objects.filter(pk = id)
+
+    return HttpResponse(
+        serializers.serialize("json", data), 
+        content_type="application/json"
+    )
+
+def show_xml_by_id(request, id):
+    data = BarangWishList.objects.filter(pk=id)
+
+    return HttpResponse(
+        serializers.serialize("xml", data), 
+        content_type="application/xml"
+    )
